fe867b36dbeb02e04da5b3fc12b460f3fd7a90de
Dubjay18 <jejeniyi7@gmail.com>
2024-04-21 00:48:43 +0100

    feat:built out tabs

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,13 +1,37 @@
 import React from 'react';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
 import { Link, Tabs } from 'expo-router';
-import { Pressable } from 'react-native';
+import {Image, Pressable, Text, View} from 'react-native';
 
 import Colors from '@/constants/Colors';
 import { useColorScheme } from '@/components/useColorScheme';
 import { useClientOnlyValue } from '@/components/useClientOnlyValue';
+import {icons} from "@/constants";
 
 // You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
+const TabIcon = ({ icon, color, name, focused }:{
+    icon: any;
+    color: string;
+    name: string;
+    focused: boolean;
+}) => {
+    return (
+        <View className="flex items-center justify-center gap-2">
+            <Image
+                source={icon}
+                resizeMode="contain"
+                tintColor={color}
+                className="w-6 h-6"
+            />
+            <Text
+                className={`${focused ? "font-psemibold" : "font-pregular"} text-xs`}
+                style={{ color: color }}
+            >
+                {name}
+            </Text>
+        </View>
+    );
+}
 function TabBarIcon(props: {
   name: React.ComponentProps<typeof FontAwesome>['name'];
   color: string;
@@ -19,41 +43,80 @@ export default function TabLayout() {
   const colorScheme = useColorScheme();
 
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        // Disable the static render of the header on web
-        // to prevent a hydration error in React Navigation v6.
-        headerShown: useClientOnlyValue(false, true),
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Tab One',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
-          headerRight: () => (
-            <Link href="/modal" asChild>
-              <Pressable>
-                {({ pressed }) => (
-                  <FontAwesome
-                    name="info-circle"
-                    size={25}
-                    color={Colors[colorScheme ?? 'light'].text}
-                    style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
-                  />
-                )}
-              </Pressable>
-            </Link>
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="bookmark"
-        options={{
-          title: 'Tab Two',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
-        }}
-      />
-    </Tabs>
+      <Tabs
+          screenOptions={{
+              tabBarActiveTintColor: "#1594d4",
+              tabBarInactiveTintColor: "#CDCDE0",
+              tabBarShowLabel: false,
+              tabBarStyle: {
+                  backgroundColor: "#161622",
+                  borderTopWidth: 1,
+                  borderTopColor: "#232533",
+                  height: 84,
+              },
+          }}
+      >
+          <Tabs.Screen
+              name="index"
+              options={{
+                  title: "Home",
+                  headerShown: false,
+                  tabBarIcon: ({ color, focused }) => (
+                      <TabIcon
+                          icon={icons.home}
+                          color={color}
+                          name="Home"
+                          focused={focused}
+                      />
+                  ),
+              }}
+          />
+          <Tabs.Screen
+              name="bookmark"
+              options={{
+                  title: "Bookmark",
+                  headerShown: false,
+                  tabBarIcon: ({ color, focused }) => (
+                      <TabIcon
+                          icon={icons.bookmark}
+                          color={color}
+                          name="Bookmark"
+                          focused={focused}
+                      />
+                  ),
+              }}
+          />
+
+          <Tabs.Screen
+              name="create"
+              options={{
+                  title: "Create",
+                  headerShown: false,
+                  tabBarIcon: ({ color, focused }) => (
+                      <TabIcon
+                          icon={icons.plus}
+                          color={color}
+                          name="Create"
+                          focused={focused}
+                      />
+                  ),
+              }}
+          />
+          <Tabs.Screen
+              name="profile"
+              options={{
+                  title: "Profile",
+                  headerShown: false,
+                  tabBarIcon: ({ color, focused }) => (
+                      <TabIcon
+                          icon={icons.profile}
+                          color={color}
+                          name="Profile"
+                          focused={focused}
+                      />
+                  ),
+              }}
+          />
+      </Tabs>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,9 +6,9 @@ module.exports = {
       colors: {
         primary: "#161622",
         secondary: {
-          DEFAULT: "#FF9C01",
-          100: "#FF9001",
-          200: "#FF8E01",
+          DEFAULT: "#1594d4",
+          100: "#11a7f2",
+          200: "#02a4f5",
         },
         black: {
           DEFAULT: "#000",
